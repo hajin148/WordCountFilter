50c3d1fbaa45d1ac0ada5550230e1e7d79127d5d
Hajin_Jeon <u1437827@umail.utah.edu>
2022-10-28 17:02:25 -0600

    Initial Commit

---
+++ wordCounter/ClassLibrary1/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace wordCounter
+{
+    public class Words
+    {
+        public string word;
+        public int count = 0;
+
+        public Words(string word)
+        {
+            this.word = word;
+        }
+    }
+
+    public class ListofWords
+    {
+        public static Dictionary<string, Words> wordClasses = new();
+        public static List<string> filter = new();
+        public ListofWords()
+        {
+
+        }
+        public ListofWords(string filename)
+        {
+            string jsonText = File.ReadAllText(filename);
+            string filterFile = File.ReadAllText("filter.txt");
+            string[] substrings = Regex.Split(jsonText, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)|(\\.)|(\\ )|(\\,)|(\\n)|(\\t)|(\\r)");
+            string[] filterstrings = Regex.Split(filterFile, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)|(\\.)|(\\ )|(\\,)|(\\n)|(\\t)|(\\r)");
+
+            foreach (string filterstring in filterstrings)
+            {
+                if(filterstring == ",")
+                {
+                    continue;
+                }
+                else
+                {
+                    filter.Add(filterstring.ToUpper());
+                }
+            }
+
+            foreach (string strings in substrings)
+            {
+                string upperString = strings.ToUpper();
+                if(filter.Contains(strings.ToUpper())|upperString == " " | upperString == "," | upperString == "." | upperString == "/" | upperString == "!" | upperString == "\"" | upperString == "\'" | upperString == "/" | upperString == "?" )
+                {
+                    continue;
+                }
+
+                else if (wordClasses.ContainsKey(upperString))
+                {
+                    wordClasses[upperString].count++;
+                    
+                    
+                }
+                else
+                {
+                    Words word = new Words(upperString);
+                    word.count += 1;
+                    if(!wordClasses.ContainsKey(upperString))
+                    {
+                        wordClasses.Add(upperString, word);
+                    }
+                    
+                }
+
+            }
+        }
+  
+
+    }
+
+    
+
+}
+
---
+++ wordCounter/Test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using wordCounter;
+
+namespace wordCounter;
+
+public class WordCounting
+{
+
+    public static void Main()
+    {
+        ListofWords a = new("test.txt");
+        Console.WriteLine(a);
+
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+
+        ListofWords result = new();
+        foreach(KeyValuePair<string, Words> ab in ListofWords.wordClasses)
+        {
+            if(ListofWords.wordClasses[ab.Key].count >= 0)
+            {
+                if(Regex.IsMatch(ab.Key, @"^[A-Za-z_]*$"))
+                {
+                    dict.Add(ab.Key.ToLower(), ListofWords.wordClasses[ab.Key].count);
+                }
+                
+            }
+        }
+
+        var sortedDict = from entry in dict orderby entry.Value descending select entry;
+
+        Console.WriteLine("-------------WORDSTARTS-------------");
+        foreach (KeyValuePair<string, int> ab in sortedDict)
+        {
+            if (ab.Key.Length > 0)
+            {
+                Console.WriteLine(ab.Key.ToUpper()[0] + ab.Key[1..]);
+            }
+        }
+
+        Console.WriteLine("-------------COUNTSTARTS-------------");
+        foreach (KeyValuePair<string, int> ab in sortedDict)
+        {
+            if (ab.Key.Length > 0)
+            {
+                Console.WriteLine(ab.Value);
+            }
+        }
+
+    }
+}
+
